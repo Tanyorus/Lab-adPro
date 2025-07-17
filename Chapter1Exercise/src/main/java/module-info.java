361b41aa9cdcfb36905b3671c096b9d63872adc4
Tanyorus <99578759+Tanyorus@users.noreply.github.com>
2025-07-17 09:01:55 +0700

    Delete Chapter1Exercise directory

--- Chapter1Exercise/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module se233.chapter1exercise {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens se233.chapter1exercise to javafx.fxml;
-    exports se233.chapter1exercise;
-}
--- Chapter1Exercise/src/main/java/se233/chapter1exercise/model/DamageType.java
+++
@@ -1,7 +0,0 @@
-package se233.chapter1exercise.model;
-
-public enum DamageType {
-    physical,
-    magical;
-
-}
